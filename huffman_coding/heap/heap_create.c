72fc6704f9a4ab118e4e8f993756d9ca74fa9aa2
Pablo <p5.tacoronte@gmail.com>
2024-09-26 14:42:13 +0000

    0

---
+++ huffman_coding/heap/heap_create.c
@@ -0,0 +1,19 @@
+#include "heap.h"
+
+
+/**
+ * heap_create- func
+ * @data_cmp: function pointer
+ * Return: heap_t
+ */
+heap_t *heap_create(int (*data_cmp)(void *, void *))
+{
+	heap_t *root = NULL;
+
+	root = malloc(sizeof(heap_s));
+	if (!root)
+		return(NULL);
+	root->size = 0;
+	root->data_cmp = data_cmp;
+	root->root = NULL;
+}
