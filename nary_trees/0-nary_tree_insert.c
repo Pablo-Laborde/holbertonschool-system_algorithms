1823537fe70bd2464710e833f69a9c535f81b669
Pablo Laborde <p5.tacoronte@gmail.com>
2024-10-27 18:37:52 -0300

    exeercise 0

---
+++ nary_trees/0-nary_tree_insert.c
@@ -0,0 +1,33 @@
+#include "nary_trees.h"
+
+
+/**
+ * nary_tree_insert- func
+ * @parent: nary_tree_t *
+ * @str: char const *
+ * Return: nary_tree_t *
+ */
+nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str)
+{
+	char *ns = NULL;
+	nary_tree_t *node = NULL;
+
+	if (!str)
+		return (NULL);
+	ns = strdup(str);
+	node = malloc(sizeof(nary_tree_t));
+	if (!ns || !node)
+		return (NULL);
+	node->content = ns;
+	node->parent = parent;
+	if (parent)
+	{
+		node->next = parent->children;
+		node->children = NULL;
+		parent->children = node;
+		parent->nb_children++;
+	}
+	else
+		node->next = NULL;
+	return (node);
+}
