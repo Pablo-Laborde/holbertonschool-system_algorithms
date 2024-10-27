8721df78956dda611674b9ab335c0b0fdaa8570b
Pablo Laborde <p5.tacoronte@gmail.com>
2024-10-27 19:58:55 -0300

    exercise 1

---
+++ nary_trees/1-nary_tree_delete.c
@@ -0,0 +1,18 @@
+#include "nary_trees.h"
+
+
+/**
+ * ary_tree_delete- func
+ * @tree: nary_tree_t *
+ * Return: void
+ */
+void nary_tree_delete(nary_tree_t *tree)
+{
+	if (tree)
+	{
+		nary_tree_delete(tree->next);
+		nary_tree_delete(tree->children);
+		free(tree->content);
+		free(tree);
+	}
+}
