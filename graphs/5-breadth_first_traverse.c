ffda271a41ce32835e9573d943bfaeeddd8f6f25
Pablo Laborde <p5.tacoronte@gmail.com>
2025-03-23 14:17:53 -0300

    fix traverse

--- graphs/5-breadth_first_traverse.c
+++ graphs/5-breadth_first_traverse.c
@@ -11,7 +11,7 @@ size_t breadth_first_traverse(const graph_t *graph
 			, void (*action)(const vertex_t *v, size_t depth))
 {
 	size_t dp = 0, *nv = NULL, i = 0;
-	vertex_t *vx = NULL;
+	queue_t *q = NULL;
 
 	if (graph)
 	{
@@ -20,8 +20,9 @@ size_t breadth_first_traverse(const graph_t *graph
 			return (0);
 		for (; i < graph->nb_vertices; i++)
 			nv[i] = 1;
-		vx = graph->vertices;
-		dp = rec_breadth(vx, action, nv, 0);
+		q = breadth_queue(graph->vertices, nv);
+		dp = breadth_queue_print(q, action);
+		breadth_queue_free(q);
 		free(nv);
 	}
 	return (dp);
@@ -29,47 +30,96 @@ size_t breadth_first_traverse(const graph_t *graph
 
 
 /**
-* rec_breadth- func
-* @vx: vertex_t *
-* @action: function pointer
-* @nv: size_t *
-* @dp: size_t
-* Return: size_t
-*/
-size_t rec_breadth(vertex_t *vx
-			, void (*action)(const vertex_t *v, size_t depth)
-			, size_t *nv, size_t dp)
+ * breadth_queue - func
+ * @vx: vertex
+ * @nv: size_t *
+ * Return: queue
+ */
+queue_t *breadth_queue(vertex_t *vx, size_t *nv)
+{
+	size_t depth = 0;
+	queue_t *q = NULL, *a = NULL;
+	edge_t *e = NULL;
+
+	if (!vx)
+		return (NULL);
+	q = malloc(sizeof(queue_t));
+	if (!q)
+		return (NULL);
+	q->dest = vx;
+	q->depth = depth;
+	q->next = NULL;
+	nv[vx->index] = 0;
+	a = q;
+	while (a)
+	{
+		e = a->dest->edges;
+		while (e)
+		{
+			if (nv[e->dest->index])
+			{
+				breadth_queue_add(a, e->dest, a->depth + 1);
+				nv[e->dest->index] = 0;
+			}
+			e = e->next;
+		}
+		a = a->next;
+	}
+	return (q);
+}
+
+
+/**
+ * breadth_queue_add - func
+ * @q: queue list
+ * @vx: vertex
+ * @depth: depth
+ */
+void breadth_queue_add(queue_t *q, vertex_t *vx, size_t depth)
+{
+	if (!q->next)
+	{
+		q->next = malloc(sizeof(queue_t));
+		if (!q->next)
+			return;
+		q->next->dest = vx;
+		q->next->depth = depth;
+		q->next->next = NULL;
+	}
+	else
+		breadth_queue_add(q->next, vx, depth);
+}
+
+
+/**
+ * breadth_queue_free - func
+ * @q: queue list
+ */
+void breadth_queue_free(queue_t *q)
+{
+	if (!q)
+		return;
+	breadth_queue_free(q->next);
+	free(q);
+}
+
+
+/**
+ * breadth_queue_print - func
+ * @q: queue list
+ * @action: func pointer
+ * Return: depth
+ */
+size_t breadth_queue_print(queue_t *q,
+	void (*action)(const vertex_t *, size_t))
 {
 	size_t a = 0, b = 0;
-	edge_t *aux = NULL;
 
-	if (vx)
+	if (q)
 	{
-		if (nv[vx->index])
-		{
-			action(vx, dp);
-			nv[vx->index] = 0;
-		}
-		aux = vx->edges;
-		while (aux)
-		{
-			if (nv[aux->dest->index])
-				action(aux->dest, dp + 1);
-			aux = aux->next;
-		}
-		aux = vx->edges;
-		while (aux)
-		{
-			if (nv[aux->dest->index])
-			{
-				nv[aux->dest->index] = 0;
-				a = rec_breadth(aux->dest, action, nv, dp + 1);
-				if (a > b)
-					b = a;
-			}
-			aux = aux->next;
-		}
-		return ((b > dp) ? b : dp);
+		action(q->dest, q->depth);
+		a = breadth_queue_print(q->next, action);
+		b = (q->depth > a) ? q->depth : a;
 	}
-	return (0);
+	return (b);
 }
--- graphs/graphs.h
+++ graphs/graphs.h
@@ -73,33 +73,51 @@
 	} graph_t;
 
 
+	/**
+	* struct queue_s - linked list
+	*
+	* @dest: Pointer to the connected vertex
+	* @next: Pointer to the next node
+	*/
+	typedef struct queue_s
+	{
+			vertex_t    *dest;
+			struct queue_s   *next;
+			size_t depth;
+	} queue_t;
+
+
 /* Functions */
-	graph_t *graph_create(void);
+graph_t *graph_create(void);
 
-	vertex_t *graph_add_vertex(graph_t *graph, const char *str);
-	vertex_t *add_rec_vertex(vertex_t *vertex, size_t index, const char *str);
-	vertex_t *create_vertex(size_t index, const char *str);
+vertex_t *graph_add_vertex(graph_t *graph, const char *str);
+vertex_t *add_rec_vertex(vertex_t *vertex, size_t index, const char *str);
+vertex_t *create_vertex(size_t index, const char *str);
 
-	int graph_add_edge(graph_t *graph, const char *src
-			, const char *dest, edge_type_t type);
-	vertex_t *get_vertex(vertex_t *vertex, const char *str);
-	edge_t *ca_edge(edge_t *list, vertex_t *dest);
+int graph_add_edge(graph_t *graph, const char *src
+		, const char *dest, edge_type_t type);
+vertex_t *get_vertex(vertex_t *vertex, const char *str);
+edge_t *ca_edge(edge_t *list, vertex_t *dest);
 
-	void graph_delete(graph_t *graph);
-	void free_vertices(vertex_t *vl);
-	void free_edges(edge_t *el);
+void graph_delete(graph_t *graph);
+void free_vertices(vertex_t *vl);
+void free_edges(edge_t *el);
 
-	size_t depth_first_traverse(const graph_t *graph
-				, void (*action)(const vertex_t *v, size_t depth));
-	size_t rec_traverse(vertex_t *vx
-			, void (*action)(const vertex_t *v, size_t depth)
-			, size_t *nv, size_t dp);
+size_t depth_first_traverse(const graph_t *graph
+		, void (*action)(const vertex_t *v, size_t depth));
+size_t rec_traverse(vertex_t *vx
+		, void (*action)(const vertex_t *, size_t)
+		, size_t *nv, size_t dp);
 
-	size_t breadth_first_traverse(const graph_t *graph
-			, void (*action)(const vertex_t *v, size_t depth));
-	size_t rec_breadth(vertex_t *vx
-			, void (*action)(const vertex_t *v, size_t depth)
-			, size_t *nv, size_t dp);
+/* exercise 5 */
+size_t breadth_first_traverse(const graph_t *graph
+	, void (*action)(const vertex_t *v, size_t depth));
+queue_t *breadth_queue(vertex_t *vx, size_t *nv);
+void breadth_queue_add(queue_t *q, vertex_t *vx, size_t depth);
+void breadth_queue_free(queue_t *q);
+size_t breadth_queue_print(queue_t *q,
+	void (*action)(const vertex_t *, size_t));
+
 
 
 #endif
