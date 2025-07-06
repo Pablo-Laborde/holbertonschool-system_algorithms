958081c1f8e43b864a25da0c8feedddbec4f9136
Pablo Laborde <p5.tacoronte@gmail.com>
2025-07-06 01:28:23 -0300

    header

---
+++ pathfinding/pathfinding.h
@@ -0,0 +1,35 @@
+#ifndef PATHFINDING_H
+#define PATHFINDING_H
+
+
+#include "queues.h"
+#include "graphs.h"
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+
+
+/**
+ * struct point_s - Structure storing coordinates
+ *
+ * @x: X coordinate
+ * @y: Y coordinate
+ */
+typedef struct point_s
+{
+	int x;
+	int y;
+} point_t;
+
+
+
+
+/* Functions */
+queue_t *backtracking_array(char **map, int rows, int cols, point_t const
+	*start, point_t const *target);
+
+
+#endif
